e14caecd2298834227ec1112eb740040a31644eb
mike-q1234 <99479083+mike-q1234@users.noreply.github.com>
2022-02-18 21:26:40 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+
+Project: County Library Management system.
+Author: Michael Maina
+Date: Fed,2022
+Compiler: GNU   GCC
+Language: C99
+License: MIT
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tCounty Library\n");
+    printf("Welcome Mr.Michael.\n");
+    printf("what would you like to do?\n");
+    printf("1. Add new User\n");
+    printf("2. Add Book.\n");
+    printf("Selected action: ");
+    return 0;
+}
